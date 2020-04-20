5fb4d3698ecabf3c5bedcb142db810e02405a9d9
han chiang <yaphc@hotmail.com>
2020-04-20 18:28:08 +0800

    Fetch data through http request

---
+++ 1-tutorial/fetch/main.go
@@ -0,0 +1,38 @@
+/**
+* Fetch prints the content found at a URL.
+* This program introduces functions from two packages, net/http and io/ioutil. The
+* http.Get function makes an HTTP request and, if there is no error, returns the result in the
+* response struct resp. The Body field of resp contains the server response as a readable
+* stream. Next, ioutil.ReadAll reads the entire response; the result is stored in b
+ */
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		// body, err := ioutil.ReadAll(resp.Body) // Reads the whole file into memory
+		_, err = io.Copy(os.Stdout, resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		// fmt.Printf("status: %s\n%s", resp.Status, resp.Body)
+	}
+}
