bc09a03b079b8e13b6824cd6d6de42f78bbb5be7
han chiang <yaphc@hotmail.com>
2020-04-21 21:48:42 +0800

    Simple web server

---
+++ 1-tutorial/server2/main.go
@@ -0,0 +1,38 @@
+// A minimal "echo" and counter server
+/*
+ * The server runs the handler for each incoming request in a separate goroutine so that
+ * it can serve multiple requests simultaneously
+ */
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+)
+
+var mu sync.Mutex
+var count int
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/count", counter)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+// echoes the Path components of the requested URL
+func handler(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	count++
+	mu.Unlock()
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
+
+// counter echoes the number of calls so far
+func counter(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	fmt.Fprintf(w, "Count %d\n", count)
+	mu.Unlock()
+}
