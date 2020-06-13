bc5ca93d9231ac43dce5f2ebd95ba286d09e9b72
Han Chiang <yaphc@hotmail.com>
2020-06-13 15:17:11 +0800

    Some composite data types such as array

--- 3-data-types/comma/main.go
+++ 3-data-types/comma/main.go
@@ -1,6 +1,11 @@
+/*
+* Adds a comma for every 3 digits of the integer, i.e. 1234 -> 1,234, 12345678 -> 12,345,678
+ */
+
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"os"
 )
@@ -13,6 +18,35 @@ func comma(s string) string {
 	return comma(s[:n-3]) + "," + s[n-3:]
 }
 
+func commaBetter(s string) string {
+	var buf bytes.Buffer
+	var commaIndex = len(s) % 3
+
+	if len(s) < 4 {
+		return s
+	}
+
+	// Handle the first block of number if input is not a multiple of 3
+	if commaIndex != 0 {
+		for i := 0; i < commaIndex; i++ {
+			buf.WriteByte(s[i])
+		}
+		buf.WriteString(",")
+	}
+
+	// Handle the rest of the blocks of number
+	var count int
+	for i := commaIndex; i < len(s); i++ {
+		if count > 0 && count%3 == 0 {
+			buf.WriteString(",")
+		}
+		buf.WriteByte(s[i])
+		count++
+	}
+	return buf.String()
+}
+
 func main() {
-	fmt.Printf(comma(os.Args[1]))
+	fmt.Println(comma(os.Args[1]))
+	fmt.Println(commaBetter(os.Args[1]))
 }
---
+++ 3-data-types/printints/main.go
@@ -0,0 +1,25 @@
+// intsToString is like fmtSprintf(values) but adds commas.package main
+
+package main
+
+import (
+	"bytes"
+	"fmt"
+)
+
+func intsToString(values []int) string {
+	var buf bytes.Buffer
+	buf.WriteByte('[')
+	for i, v := range values {
+		if i > 0 {
+			buf.WriteString(", ")
+		}
+		fmt.Fprintf(&buf, "%d", v)
+	}
+	buf.WriteByte(']')
+	return buf.String()
+}
+
+func main() {
+	fmt.Println(intsToString([]int{1, 2, 3}))
+}
