ede2dfaccad271b5ee3d775a1fd846301920ee29
Han Chiang <yaphc@hotmail.com>
2020-06-19 14:08:57 +0800

    Buffered and unbuffered channel

---
+++ 8-goroutines/pipeline1/main.go
@@ -0,0 +1,34 @@
+/**
+* Demonstrates the piping of an output of a goroutine to the input of another goroutine
+ */
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	naturals := make(chan int)
+	squares := make(chan int)
+
+	// Counter
+	go func() {
+		for x := 0; ; x++ {
+			naturals <- x
+		}
+	}()
+
+	// Squarer
+	go func() {
+		for {
+			x := <-naturals
+			squares <- x * x
+		}
+	}()
+
+	// Printer (in main goroutine)
+	for {
+		fmt.Println(<-squares)
+	}
+}
