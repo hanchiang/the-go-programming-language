0ba80458d8c96e4d7c3815c77953c9a26c88686b
Han Chiang <yaphc@hotmail.com>
2020-06-21 16:29:50 +0800

    Rename file, add comment

--- 11-testing/word1/word_test.go
+++ 11-testing/word1/word_test.go
@@ -16,12 +16,14 @@ func TestNonPalindrome(t *testing.T) {
 	}
 }
 
+// oops! failure
 func TestFrenchPalindrome(t *testing.T) {
 	if !IsPalindrome("été") {
 		t.Error(`IsPalindrome("été") = false`)
 	}
 }
 
+// oops! failure
 func TestCanalPalindrome(t *testing.T) {
 	input := "A man, a plan, a canal: Panama"
 	if !IsPalindrome(input) {
